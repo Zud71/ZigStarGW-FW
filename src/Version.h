ba0385e788034f2b9ce5cb49448c71031d5f4627
Zud71 <other-mail@bk.ru>
2022-11-25 12:33:17 +0300

    Перевод. Кнопка обновлений

--- src/Version.h
+++ src/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.2"
+        #define VERSION "0.1.3"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-11-24 20:00:00.540593"
+        #define BUILD_TIMESTAMP "2022-11-25 12:00:00.540593"
     #endif
     
--- src/websrc/js/functions.js
+++ src/websrc/js/functions.js
@@ -171,3 +171,4 @@ function getLatestReleaseInfo() {
 	  $("#releaseinfo").fadeIn("slow");
 	});
   }
+
